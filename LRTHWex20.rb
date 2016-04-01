365dbd94ba4fb972d838543cf70deb2d973e8ba1
jm-rives <joanna.rives@gmail.com>
2016-03-31 17:58:57 -0700

    ADD File

---
+++ LRTHWex20.rb
@@ -0,0 +1,38 @@
+# LRTHWex20.rb
+
+input_file = ARGV.first
+
+def print_all(f)
+	puts f.read
+end
+
+def rewind(f)
+	f.seek(0)
+end
+
+def print_a_line(line_count, f)
+	puts "#{line_count}, #{f.gets.chomp}"
+end
+
+current_file = open(input_file)
+
+puts "First let's print the whole file:\n"
+
+print_all(current_file)
+
+puts "Now let's rewind, kind of like a tape."
+
+# have to have this, otherwise no bueno 
+rewind(current_file)
+
+puts "Let's print three lines:"
+
+current_line = 1
+print_a_line(current_line, current_file)
+
+current_line = current_line + 1
+print_a_line(current_line, current_file)
+
+# going off piece to see if this works # werks fine
+current_line += 1
+print_a_line(current_line, current_file)
